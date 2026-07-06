3de727a05ebb4510d76937b056d09e673cf28d49
Maru979182063 <157974931+Maru979182063@users.noreply.github.com>
2026-07-06 13:59:45 +0800

    Add pipeline core

---
+++ __pdf2ppt/src/lib/process-utils.mjs
@@ -0,0 +1,32 @@
+import { spawn } from 'node:child_process';
+
+export async function runCommand(command, args, options = {}) {
+  return new Promise((resolve, reject) => {
+    const child = spawn(command, args, {
+      stdio: ['ignore', 'pipe', 'pipe'],
+      ...options,
+    });
+
+    let stdout = '';
+    let stderr = '';
+    child.stdout.on('data', (chunk) => {
+      stdout += chunk.toString();
+    });
+    child.stderr.on('data', (chunk) => {
+      stderr += chunk.toString();
+    });
+
+    child.on('error', reject);
+    child.on('close', (code) => {
+      if (code === 0) {
+        resolve({ stdout, stderr });
+      } else {
+        reject(
+          new Error(
+            `Command failed (${command} ${args.join(' ')}), code=${code}\nstdout=${stdout}\nstderr=${stderr}`
+          )
+        );
+      }
+    });
+  });
+}
